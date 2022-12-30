c86c27ae30a9fa7da984fd94a3e5e03c263daf18
kubeworkz <kubeworkz@gmail.com>
2022-12-30 18:34:59 -0500

    remove separator

--- src/auth/LocalLogin.tsx
+++ src/auth/LocalLogin.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import { ENV } from '@cloudrock/configs/default';
-import { translate } from '@cloudrock/i18n';
-
 import './LocalLogin.css';
 
 import { SigninForm } from './SigninForm';
@@ -11,23 +8,11 @@ interface LocalLoginProps {
   enableSeperator: boolean;
 }
 
-const Border = () => (
-  <div
-    className="LoginSeparatorBorder"
-    style={{
-      borderBottomColor: ENV.plugins.CLOUDROCK_CORE.BRAND_COLOR,
-    }}
-  />
-);
-
 export const LocalLogin: React.FC<LocalLoginProps> = ({ enableSeperator }) => (
   <>
     <SigninForm />
     {enableSeperator && (
       <div className="LoginSeparator">
-        <Border />
-        <div className="LoginSeparatorText">{translate('OR')}</div>
-        <Border />
       </div>
     )}
   </>
